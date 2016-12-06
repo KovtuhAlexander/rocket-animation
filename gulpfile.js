a3cc2f0366a1e7c726b726b51354236817e78033
KovtuhAlexander <crytek.kovt@gmail.com>
2016-12-06 19:59:37 +0200

    cleaning

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('browser-sync', function() {
 
 gulp.task('scripts', function() {
     return gulp.src([
-        
+
         ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
